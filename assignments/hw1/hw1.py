97e358248c2b6ec700937a4c58bf700bfd3e35a0
reedms58 <reedms@g.cofc.edu>
2022-01-20 20:06:01 -0500

    Completed hw 1

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,34 +1,47 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Maddie Reed
+hw1.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: This program calculates the area of a rectangle, the volume of a rectangular prism, percentages,
+ coffee prices, and converts kilometers to miles.
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
 def calc_rec_area():
-    pass
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    area = length*width
+    print("Area=", area)
 
 
 def calc_volume():
-    pass
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    height = eval(input("Enter the height: "))
+    volume = length*width*height
+    print("Volume=", volume)
 
 
 def shooting_percentage():
-    pass
+    total_shots = eval(input("Enter the player's total shots: "))
+    shots_made = eval(input("Enter how many shots the player made: "))
+    percentage = (shots_made/total_shots)*100
+    print("Shooting Percentage: ", percentage, "%")
 
 
 def coffee():
-    pass
+    pounds_of_coffee = eval(input("How many pounds of coffee would you like? "))
+    total = pounds_of_coffee*(10.5+0.86)+1.5
+    print("You're total is: ", "$", total)
 
 
 def kilometers_to_miles():
-    pass
+    kilometers = eval(input("How many kilometers did you travel? "))
+    miles = kilometers/1.6
+    print("That's", miles, "miles!")
 
 
 if __name__ == '__main__':
