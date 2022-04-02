5474af964f7f15c6f63760c4ac1cf8b00df9a2ac
reedms58 <reedms@g.cofc.edu>
2022-04-01 23:11:31 -0400

    completed hw9

--- assignments/hw9/hw9.py
+++ assignments/hw9/hw9.py
@@ -9,6 +9,7 @@ I certify that this assignment is entirely my own work.
 """
 
 import random
+from graphics import *
 
 
 def get_words(file_name):
@@ -16,14 +17,14 @@ def get_words(file_name):
     words = []
     file_words = my_file.readlines()
     for word in file_words:
-        word = word.replace('\n', '')
         words.append(word)
     return words
 
 
 def get_random_word(words):
-    num = random.randint(0, len(words))
+    num = random.randint(0, len(words)-1)
     word = words[num]
+    word = word.replace('\n', '')
     print(word)
     return word
 
@@ -31,10 +32,8 @@ def get_random_word(words):
 def letter_in_secret_word(letter, secret_word):
     letter_num = secret_word.count(letter)
     if letter_num > 0:
-        print("yes")
         return True
     else:
-        print("no")
         return False
 
 
@@ -57,20 +56,94 @@ def make_hidden_secret(secret_word, guesses):
 
 
 def won(guessed):
-    if guessed.count("_") >= 0:
+    if guessed.count("_") <= 0:
         return True
     else:
         return False
 
 
 def play_graphics(secret_word):
-    pass
+    win = GraphWin("Hangman", 500, 700)
+    guessed = []
+
+    head = Circle(Point(200, 145), 20)
+    body = Line(Point(200, 165), Point(200, 365))
+    r_leg = Line(Point(200, 365), Point(250, 500))
+    l_leg = Line(Point(200, 365), Point(150, 500))
+    r_arm = Line(Point(200, 200), Point(250, 275))
+    l_arm = Line(Point(200, 200), Point(150, 275))
+
+    man = [head, body, r_leg, l_leg, r_arm, l_arm]
+
+    foot = Line(Point(150, 550), Point(350, 550))
+    pole = Line(Point(300, 550), Point(300, 100))
+    top = Line(Point(300, 100), Point(200, 100))
+    rope = Line(Point(200, 100), Point(200, 125))
+    foot.draw(win)
+    pole.draw(win)
+    top.draw(win)
+    rope.draw(win)
+    prompt = Text(Point(75, 680), "Guess a letter:")
+    prompt.draw(win)
+
+    guessed_txt = Text(Point(450, 250), guessed)
+    guessed_txt.draw(win)
+
+    num_guess = 6
+    num_guessed = 0
+    word_prog = make_hidden_secret(secret_word, guessed)
+    while num_guess > 0 and not won(word_prog):
+        num_guess = 6 - num_guessed
+
+        secret_prog = Text(Point(250, 630), word_prog)
+        secret_prog.draw(win)
+
+        enter_let = Entry(Point(250, 680), 3)
+        enter_let.draw(win)
+        win.getMouse()
+        letter = enter_let.getText()
+        guessed = guessed + [letter]
+        guess_len = len(guessed)
+        guessed_txt.setText(guessed[0:guess_len])
+
+        word_prog = make_hidden_secret(secret_word, guessed)
+        if not letter_in_secret_word(letter, secret_word):
+            man[num_guessed].draw(win)
+            num_guessed = num_guessed + 1
+
+    if won(word_prog):
+        winner = Text(Point(250, 50), "winner!")
+        winner.draw(win)
+
+    else:
+        loser = Text(Point(250, 50), "you loose!"+" the word was {}".format(secret_word))
+        loser.draw(win)
+
+    win.getMouse()
+    win.close()
 
 
 def play_command_line(secret_word):
-    quessed = '_'
-    while not won(guessed):
-        print()
+    guessed = []
+    num_guess = 6
+    num_guessed = 0
+    word_prog = make_hidden_secret(secret_word, guessed)
+    while num_guess > 0 and not won(word_prog):
+        num_guess = 6 - num_guessed
+        print(guessed)
+        print("guesses remaining: ", num_guess)
+        print(word_prog)
+        letter = input("guess a letter: ")
+        guessed = guessed + [letter]
+        word_prog = make_hidden_secret(secret_word, guessed)
+        if not letter_in_secret_word(letter, secret_word):
+            num_guessed = num_guessed + 1
+    if won(word_prog):
+        print("winner!")
+        print("the word is:", secret_word)
+    else:
+        print("you loose!")
+        print("the word was:", secret_word)
 
 
 if __name__ == '__main__':
